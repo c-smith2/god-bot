a648288c2250f3f200e325031fd47c1e33523f8d
c-smith2 <39007371+c-smith2@users.noreply.github.com>
2018-05-06 13:18:00 +1200

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ client.on("ready", () => {
   
   console.log("Bot Launched...")
   client.user.setStatus('Online')
-  client.user.setPresence({game:{name:'Small children from the window',type:3}});
+  client.user.setPresence({game:{name:'You wank at night',type:3}});
 });
 
 client.login(process.env.BOT_TOKEN);
