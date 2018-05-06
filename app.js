3449f9bcce8b10cd69145ecc11a01d48e84f9ab0
c-smith2 <39007371+c-smith2@users.noreply.github.com>
2018-05-06 22:24:38 +1200

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ client.on("ready", () => {
   
   console.log("Bot Launched...")
   client.user.setStatus('Online')
-  client.user.setPresence({game:{name:'Sanraj fuck his cousin',type:3}});
+  client.user.setPresence({game:{name:'you wank at night',type:3}});
 });
 
 client.login(process.env.BOT_TOKEN);
