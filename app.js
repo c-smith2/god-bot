12149bc0fe6f434f2ad3d20faf5b0b637d851576
c-smith2 <39007371+c-smith2@users.noreply.github.com>
2018-05-06 12:25:56 +1200

    Create app.js

---
+++ app.js
@@ -0,0 +1,11 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+
+client.on("ready", () => {
+  
+  console.log("Bot Launched...")
+  client.user.setStatus('Online')
+  client.user.setPresence({game:{name:'Sanraj sleep ;)',type:3}});
+});
+
+client.login(process.env.NDQyMzUxOTA2MTQ5NjI5OTUy.Dc9mkg.fWCJJCnL9-U8kkDx-RW6QNjFvkQ);
