8b037ecc09213ca6be5aac2dab60265d1bac13f9
c-smith2 <39007371+c-smith2@users.noreply.github.com>
2018-05-11 20:00:40 +1200

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ client.on("ready", () => {
   
   console.log("Bot Launched...")
   client.user.setStatus('Online')
-  client.user.setPresence({game:{name:'some kid have a seizure',type:3}});
+  client.user.setPresence({game:{name:'gorillas ejaculate on cats',type:3}});
 });
 
 client.login(process.env.BOT_TOKEN);
