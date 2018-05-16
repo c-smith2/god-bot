b731ace9361bf8f92dfd5c14243307bcd5771c33
c-smith2 <39007371+c-smith2@users.noreply.github.com>
2018-05-16 21:19:41 +1200

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ client.on("ready", () => {
   
   console.log("Bot Launched...")
   client.user.setStatus('Online')
-  client.user.setPresence({game:{name:'you',type:3}});
+  client.user.setPresence({game:{name:'Stefan beg for admin',type:3}});
 });
 
 client.login(process.env.BOT_TOKEN);
